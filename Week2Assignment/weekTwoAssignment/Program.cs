69be4236ba136fea84e327a2df425862fb825b0b
Nnadozie Opara <nnadozieopara@yahoo.com>
2022-03-21 10:17:06 +0100

    Assignment update for the quadratic equation

--- Week2Assignment/weekTwoAssignment/Program.cs
+++ Week2Assignment/weekTwoAssignment/Program.cs
@@ -6,6 +6,12 @@ namespace weekTwoAssignment
     {
         static void Main(string[] args)
         {
+            //AssignmentOne();
+            FixedQuadratic();
+            DynamicQuadratic();
+        }
+        static void AssignmentOne()
+        {
             //Assignment 3
             int nExponent = 0;
             int wExponent = 0;
@@ -14,7 +20,7 @@ namespace weekTwoAssignment
             int lValue = 0;
             double firstTerm = (sValue * lValue) / fValue;
             double secondTerm = (20 / fValue) ^ wExponent;
-            double yValue = lValue - ((fValue ^ nExponent) * (firstTerm + secondTerm)) / (20 ^nExponent);
+            double yValue = lValue - ((fValue ^ nExponent) * (firstTerm + secondTerm)) / (20 ^ nExponent);
 
             //Assignment 4
 
@@ -51,5 +57,55 @@ namespace weekTwoAssignment
                 Console.WriteLine("false");
             }
         }
+
+        static void FixedQuadratic()
+        {
+            Console.WriteLine("what kind of root does the equation X^2 + 6X + 5 = 0 have?");
+            int a = 1;
+            int b = 6;
+            int c = 5;
+            double discriminant = (b ^ 2) - (4 * a * c);
+            if(discriminant > 0)
+            {
+                Console.WriteLine("real roots");
+            }
+            else if (discriminant < 0)
+            {
+                Console.WriteLine("complex roots");
+            }
+            else
+            {
+                Console.WriteLine("equal roots");
+            }
+        }
+
+        static void DynamicQuadratic()
+        {
+            Console.WriteLine("This function tells you the nature of the roots of your input quadratic equation \n" +
+                "Your quadratic equation is of the form aX^2 + bX + c = 0 ");
+            Console.Write("enter the value of a:");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("enter the value of b:");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("enter the value of c:");
+            double c = double.Parse(Console.ReadLine());
+            double discriminant = (b * b) - (4 * a * c);
+
+            if (discriminant > 0)
+            {
+                Console.WriteLine("real roots"); ;
+            }
+            else if (discriminant < 0)
+            {
+                Console.WriteLine("complex roots");
+            }
+            else
+            {
+                Console.WriteLine("equal roots"); ;
+            }
+        }
     }
+
+ 
 }
+
